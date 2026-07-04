6272f8443ac2223cacfc1c76b83aa2b04b9f7975
Shaunak Badani <shaunak.badani@duke.edu>
2026-07-04 11:06:58 -0700

    Add constants file

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import { basePath } from './lib/constants';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  images: { unoptimized: true },
+  basePath: basePath
 };
 
 export default nextConfig;
