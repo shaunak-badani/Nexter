c3529e21680dfdd9969f66e2148025c371f170d7
Shaunak Badani <shaunak.badani@duke.edu>
2026-07-18 09:44:11 -0700

    Building part 1 of the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
 import Image from "next/image";
-import styles from "./page.scss";
+import "./page.scss";
+import "./sidebar.scss";
+import "./features.scss";
+import "./footer.scss";
+import "./gallery.scss";
+import "./header.scss";
+import "./realtors.scss";
+import "./sidebar.scss";
+import "./story.scss";
 
 export default function Home() {
   return (
     <div className="container">
-      TEST TEXT
+      <div className="sidebar">
+        Sidebar
+      </div>
+      <header className="header">
+        Header
+      </header>
+      <div className="realtors">
+        Top 3 realtors
+      </div>
+      <section className="sectionFeatures">
+        <div className="storyPictures">
+          Story pictures
+        </div>
+        <div className="storyContent">
+          Story content
+        </div>
+        <section className="homes">
+          Homes
+        </section>
+        <section className="gallery">
+          Gallery
+        </section>
+        <footer className="footer">
+          Footer
+        </footer>
+      </section>
     </div>
   );
 }
